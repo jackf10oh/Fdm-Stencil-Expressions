c2d819ba659bd57cafaf2a1cb694439b1bbbc305
Jack Feds <jackf10oh@gmail.com>
2025-12-12 13:19:33 -0500

    GTest file started for Outer directory Fdm-Stencil-Expressions.

---
+++ tests/tests.cpp
@@ -0,0 +1,19 @@
+// tests.cpp
+//
+// FdmStencilExpressions Test suite with GTest
+//
+// JAF 12/5/2025
+
+#include<iostream>
+#include<vector>
+#include<eigen3/Eigen/Core>
+#include<eigen3/Eigen/Sparse>
+#include<gtest/gtest.h>
+#include<gmock/gmock.h>
+
+#include "../incl/All.hpp"
+
+int main()
+{
+  std::cout << "Hello world from test suite!" << std::endl; 
+}
