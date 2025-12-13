e63d34320f17d8f36b43dd41937dd16250e5e264
Jack Feds <jackf10oh@gmail.com>
2025-12-12 21:30:24 -0500

    test script for new stateful forncalc

--- test.cpp
+++ test.cpp
@@ -7,37 +7,51 @@
 #include<iostream>
 #include<iomanip>
 #include<vector>
-#include<eigen3/Eigen/Dense>
+#include<chrono>
+#include<eigen3/Eigen/Core>
+#include<eigen3/Eigen/Sparse>
+#include<eigen3/Eigen/SparseLU>
 #include<eigen3/unsupported/Eigen/KroneckerProduct>
 
 #include "incl/All.hpp"
 #include "LinOps/All.hpp"
+
+#include "incl/Utilities/FornbergCalc.hpp"
 
 using std::cout, std::endl;
 
 int main()
 {
   std::cout << std::setprecision(2); 
-  auto my_mesh = make_mesh(0.0,4.0,5); 
+  // auto my_mesh = make_mesh(0.0,5.0,6); 
 
-  Discretization1D my_vals;
-  // auto func = [](double x){return 2*x*x*x-5*x*x+3*x-1;}; // 2x^3 - 5x^2 + 3x -1 
-  auto func = [](double x){return x*x;}; // x^2 
-  my_vals.set_init(my_mesh, func); 
+  // Discretization1D my_vals;
+  // // auto func = [](double x){return 2*x*x*x-5*x*x+3*x-1;}; // 2x^3 - 5x^2 + 3x -1 
+  // auto func = [](double x){return x*x;}; // x^2 
+  // my_vals.set_init(my_mesh, func); 
 
-  TOp t(my_mesh); // TOp inherits from coeffopbase 
-  t.SetTime(104.0);
-  NthDerivOp D(my_mesh,1); // 1st derivative operator
+  using std::chrono::system_clock;
+  using std::chrono::duration_cast;
+  using Time_t = std::chrono::microseconds; 
+  auto time_lambda = [](const MeshPtr_t& m){
+    // TEST 1 
+    std::cout << "------------" << std::endl; 
+    NthDerivOp D(1); 
+    auto start = system_clock::now(); 
+    D.set_mesh(m);
+    auto end = system_clock::now(); 
+    auto d = duration_cast<Time_t>(end-start); 
+    std::cout << "Mesh size:" << m->size() << " time:"  << d.count() << std::endl; 
+  };
 
-  // cout << D.GetMat() << endl; 
+  auto mesh01 = make_mesh(0.0,1e3,1e3 + 1); 
+  auto mesh02 = make_mesh(0.0,1e4,1e4 + 1); 
+  auto mesh03 = make_mesh(0.0,1e5,1e5 + 1); 
+  auto mesh04 = make_mesh(0.0,1e6,1e6 + 1); 
 
-  auto expr = D.compose(t.compose(D)); 
-  cout << expr.GetMat() << endl; 
-
-  // auto D2 = D.compose(D); 
-  // cout << D2.GetMat() << endl; 
-
-  auto scaled = 40.0 * D; 
-  cout << D.compose(scaled).GetMat() << endl; 
+  time_lambda(mesh01);
+  time_lambda(mesh02);
+  time_lambda(mesh03);
+  time_lambda(mesh04);
 
 }
