7602a230b388990fc9b769b4700891bcdeb8a9d0
Jack Feds <jackf10oh@gmail.com>
2025-12-29 15:22:48 -0500

    BoundaryCond changed to abstract base class

--- DiffOps/BoundaryCond.hpp
+++ DiffOps/BoundaryCond.hpp
@@ -12,37 +12,37 @@
 #include "../LinOps/Mesh.hpp"
 
 // forward declaration -> type alias 
-class BoundaryCond; 
+class IBoundaryCond; 
 using MatrixStorage_t = Eigen::SparseMatrix<double, Eigen::RowMajor>; 
-using BcPtr_t = std::shared_ptr<BoundaryCond>; 
+using BcPtr_t = std::shared_ptr<IBoundaryCond>; 
 // using BcPtr_t = BoundaryCond*; // unlikely to use. Only going to assign to boundary conditions once 
 
 // Base Class for Boundary Conditions. All operators make no changes to stencil / solution 
-class BoundaryCond
+class IBoundaryCond
 {
   protected:  
     // member data 
     double m_current_time;
   public:
     // Constructors ---------------------------------------------
-    BoundaryCond(): m_current_time(0.0){}; 
+    IBoundaryCond(): m_current_time(0.0){}; 
     // Destructors ----------------------------------------------
-    virtual ~BoundaryCond()=default; 
+    virtual ~IBoundaryCond()=default; 
     // Member Funcs ----------------------------------------------
     // Set Time of current boundary condition
     virtual void SetTime(double t){m_current_time=t;};
 
     // change first/last (left/right boundary) row of the fdm stencil matrix
-    virtual void SetStencilL(MatrixStorage_t& Mat, const MeshPtr_t& mesh)const{}; 
-    virtual void SetStencilR(MatrixStorage_t& Mat, const MeshPtr_t& mesh)const{};
+    virtual void SetStencilL(MatrixStorage_t& Mat, const MeshPtr_t& mesh)const=0; 
+    virtual void SetStencilR(MatrixStorage_t& Mat, const MeshPtr_t& mesh)const=0;
     
     // change the first/last entries in an impicit solution vector 
-    virtual void SetImpSolL(Discretization1D& Sol)const{};
-    virtual void SetImpSolR(Discretization1D& Sol)const{};
+    virtual void SetImpSolL(Discretization1D& Sol)const=0;
+    virtual void SetImpSolR(Discretization1D& Sol)const=0;
 
     // change the first/last (left/right boundary) entry of a vector  
-    virtual void SetSolL(Discretization1D& Sol)const{};
-    virtual void SetSolR(Discretization1D& Sol)const{};
+    virtual void SetSolL(Discretization1D& Sol)const=0;
+    virtual void SetSolR(Discretization1D& Sol)const=0;
 };
 
 #endif // BoundaryCond.hpp
