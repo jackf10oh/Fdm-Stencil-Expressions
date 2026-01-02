8e987d14ca361d0107da2265708c95fb4eddb26a
Jack Feds <jackf10oh@gmail.com>
2026-01-02 17:34:59 -0500

    SparseDiagExpr.hpp file added. make_SparseDiag() helper function working.

---
+++ DiffOps/Utilities/SparseDiagExpr.hpp
@@ -0,0 +1,121 @@
+// make_diagonal.hpp
+//
+// takes a Eigen::SparseMatrix<double,Eigen::RowMajor> matrix of rows==1 and makes it into a diagonal matrix 
+// tightly based on eigen docs for make circulant
+// https://libeigen.gitlab.io/eigen/docs-5.0/TopicNewExpressionType.html
+// 
+// JAF 1/2/2026 
+
+#include<Eigen/Core>
+#include<Eigen/Sparse>
+#include "../FdmPlugin.hpp"
+
+// forward declaration -----------------------------------------------
+template <class ArgType>
+class SparseDiag;
+
+// type traits =======================================================================
+namespace Eigen {
+namespace internal {
+template <class ArgType>
+struct traits<SparseDiag<ArgType> > {
+  typedef Eigen::Sparse StorageKind;
+  typedef Eigen::MatrixXpr XprKind;
+  typedef typename ArgType::StorageIndex StorageIndex;
+  typedef typename ArgType::Scalar Scalar;
+  enum {
+    Flags = Eigen::RowMajor,
+    RowsAtCompileTime = ArgType::RowsAtCompileTime,
+    ColsAtCompileTime = ArgType::ColsAtCompileTime,
+    MaxRowsAtCompileTime = ArgType::MaxRowsAtCompileTime,
+    MaxColsAtCompileTime = ArgType::MaxColsAtCompileTime
+  };
+};
+}  // namespace internal
+}  // namespace Eigen
+
+// expression class ======================================================================= 
+template <class ArgType>
+class SparseDiag : public Eigen::SparseMatrixBase<SparseDiag<ArgType> > {
+  public:
+    // typedefs 
+    typedef typename Eigen::internal::ref_selector<SparseDiag>::type Nested;
+    typedef Eigen::Index Index;
+    typedef typename Eigen::internal::ref_selector<ArgType>::type ArgTypeNested;
+    
+    // constructors 
+    SparseDiag(const ArgType& arg) : m_arg(arg) {
+      static_assert( std::is_same<ArgType, MatrixStorage_t>::value, "Invalid arg type. SparseDiag must be constructed from MatrixStorage_t."); 
+      if(arg.rows()!=1) throw std::invalid_argument("SparseDiag must be constructed from MatrixStorage_t."); 
+    }
+    
+    // member functions 
+    Index rows() const { return m_arg.cols(); }
+    Index cols() const { return m_arg.cols(); }
+
+    // member data 
+    ArgTypeNested m_arg;
+  
+};
+
+// the evaluator =======================================================================
+namespace Eigen {
+namespace internal {
+template <typename ArgType>
+struct evaluator<SparseDiag<ArgType> > : evaluator_base<SparseDiag<ArgType> > {
+
+  // typedefs -------------------------------------------------- 
+  typedef SparseDiag<ArgType> XprType;
+  typedef typename nested_eval<ArgType, XprType::ColsAtCompileTime>::type ArgTypeNested;
+  typedef typename remove_all<ArgTypeNested>::type ArgTypeNestedCleaned;
+  typedef typename XprType::CoeffReturnType CoeffReturnType;
+  typedef typename XprType::Index Index; 
+  typedef typename XprType::Scalar Scalar; 
+
+  // custom InnerIterator ----------------------------------
+  struct InnerIterator{
+    // constructor 
+    InnerIterator(const evaluator& eval, Index row_idx)
+      : m_eval(eval), m_row(row_idx)
+    {
+      // iterator is only valid if row_idx is in bounds and underlying row has nonzero entry 
+      m_valid = (m_row < m_eval.rows()) && (m_eval.m_argImpl.coeff(0,m_row) != Scalar(0.0)); 
+    };
+    // member data  
+    operator bool() const { return m_valid; }; 
+    void operator++(){ m_valid=false; }; 
+    Index row() const { return m_row; }; 
+    Index col() const { return m_row; };
+    Index index() const { return m_row; };
+    Scalar value() const { return m_eval.m_argImpl.coeff(0,m_row); };
+    // member data
+    const evaluator& m_eval; 
+    Index m_row;    
+    bool m_valid; 
+  }; 
+
+  // flags ------------------------------------------------------
+  enum { CoeffReadCost = evaluator<ArgTypeNestedCleaned>::CoeffReadCost, Flags = Eigen::RowMajor };
+ 
+  // constructors ------------------------------------------------------ 
+  evaluator(const XprType& xpr) 
+    : m_argImpl(xpr.m_arg), m_rows(xpr.rows()), m_cols(xpr.cols())
+  {};
+ 
+  // member functions ------------------------------------------------------
+  Index rows() const {return m_rows; }; 
+  Index cols() const {return m_cols; }; 
+ 
+  // member data ------------------------------------------------------
+  evaluator<ArgTypeNestedCleaned> m_argImpl;
+  Index m_rows; 
+  Index m_cols; 
+};
+}  // namespace internal
+}  // namespace Eigen
+
+// the entry point ======================================================================= 
+template <class ArgType>
+SparseDiag<ArgType> make_SparseDiag(const Eigen::SparseMatrixBase<ArgType>& arg) {
+  return SparseDiag<ArgType>(arg.derived());
+}
--- tests.cpp
+++ tests.cpp
@@ -9,13 +9,14 @@
 #include<iomanip>
 #include<vector>
 #include<Eigen/Dense>
-// #include<eigen3/unsupported/Eigen/KroneckerProduct>
+#include<unsupported/Eigen/KroneckerProduct>
 #include "DiffOps/All.hpp"
 #include "LinOps/All.hpp"
 
 // #include "LinOpsXD/MeshXD.hpp"
 // #include "LinOpsXD/LinOpXDTraits.hpp"
-#include "DiffOps/Utilities/make_diagonal.hpp"
+#include "DiffOps/Utilities/CirculantExpr.hpp"
+#include "DiffOps/Utilities/SparseDiagExpr.hpp"
 
 using std::cout, std::endl;
 
@@ -34,59 +35,46 @@ int main()
 {
   std::cout << std::setprecision(2); 
 
-  // create mesh 
-  auto my_mesh = make_mesh(); 
+  // create mesh + boundary condition 
+  auto my_mesh = make_mesh(0,4,6); 
+  BcPtr_t bc = std::make_shared<NeumannBC>(); 
+
+  // first deriv stencil 
+  auto D = MatrixStorage_t(my_mesh->size(), my_mesh->size());
+  bc->SetStencilL(D,my_mesh); 
+  bc->SetStencilR(D,my_mesh); 
 
   // create flat 1 row matrix 
-  // MatrixStorage_t A(1,my_mesh->size());
+  MatrixStorage_t A(1,my_mesh->size());
 
-  // cout << "------------" << endl; 
+  // use a BC to set left/right end of A 
+  // set left side of row 
+  bc->SetStencilL(A, my_mesh); 
+  // store into a temp vector 
+  std::vector<double> temp(A.valuePtr(), A.valuePtr()+A.nonZeros());
+  // set right side of row. this zeros left side 
+  bc->SetStencilR(A, my_mesh);
+  // repopulate left side 
+  for(auto i=0; i<temp.size(); i++) A.coeffRef(0,i) = temp[i]; 
+
+  cout << "------------" << endl; 
   // cout << A << endl; 
 
+  cout << "------------" << endl; 
+  auto foo = make_SparseDiag(A); 
+  cout << foo.eval() << endl; 
 
-  // // use a BC to set left/right end of A 
-  // BcPtr_t bc = std::make_shared<NeumannBC>(); 
+  cout << "------------" << endl; 
+  MatrixStorage_t I(my_mesh->size(), my_mesh->size()); 
+  I.setIdentity();  
+  // put boundary conditions on outter dimension 
+  cout <<  Eigen::KroneckerProductSparse(foo,I) << endl;
 
-  // // set left side of row 
-  // bc->SetStencilL(A, my_mesh); 
+  cout << "------------" << endl; 
+  // put the boundary conditions on inner dimension 
+  cout << Eigen::KroneckerProductSparse(I,D) << endl; 
 
-  // // store into a temp vector 
-  // std::vector<double> temp(A.valuePtr(), A.valuePtr()+A.nonZeros());
-
-  // // set right side of row. this zeros left side 
-  // bc->SetStencilR(A, my_mesh);
-  // // repopulate left side 
-  // for(auto i=0; i<temp.size(); i++) A.coeffRef(0,i) = temp[i]; 
-
-  // cout << "------------" << endl; 
-  // cout << A << endl; 
-
-  Eigen::VectorXd vec(4);
-  vec << 1, 2, 4, 8;
-  Eigen::MatrixXd mat;
-  mat = makeCirculant(vec);
-  std::cout << mat << std::endl;
 };
-
-  // std::vector<std::pair<double,double>> axes = {{0.0,1.0},{0.0,2.0},{0.0,3.0}}; 
-  // std::vector<std::size_t> nsteps = {3,5,7}; 
-  // MeshXDPtr_t my_mesh = std::make_shared<MeshXD>(axes,nsteps); 
-
-  // cout << "my_mesh product_size: " << my_mesh->sizes_product() << endl; 
-
-  // // how to iterate through dynamic multi dim meshes?
-  // std::size_t end = my_mesh->sizes_product();  
-  // std::vector<double> coords(my_mesh->dims()); 
-  // for(std::size_t flat_i=0; flat_i<end; flat_i++){
-  //   for(std::size_t dim=0; dim<my_mesh->dims(); dim++){
-  //     std::size_t dim_i = flat_i;
-  //     dim_i /= my_mesh->sizes_partial_product(dim); 
-  //     dim_i %= my_mesh->dim_size(dim); 
-  //     cout << dim_i; 
-  //     cout << ", ";
-  //   }
-  //   cout << endl; 
-  // }
 
   // auto my_mesh = make_mesh(0.0,3.0,4); 
   // NthDerivOp D(my_mesh); 
@@ -116,112 +104,22 @@ int main()
   //   cout << val_start[i] << endl; 
   // }
 
+  // std::vector<std::pair<double,double>> axes = {{0.0,1.0},{0.0,2.0},{0.0,3.0}}; 
+  // std::vector<std::size_t> nsteps = {3,5,7}; 
+  // MeshXDPtr_t my_mesh = std::make_shared<MeshXD>(axes,nsteps); 
 
-// printed output 
-/*
-0, 0, 0, 
-1, 0, 0, 
-2, 0, 0, 
-0, 1, 0, 
-1, 1, 0, 
-2, 1, 0, 
-0, 2, 0, 
-1, 2, 0, 
-2, 2, 0, 
-0, 3, 0, 
-1, 3, 0, 
-2, 3, 0, 
-0, 4, 0, 
-1, 4, 0, 
-2, 4, 0, 
-0, 0, 1, 
-1, 0, 1, 
-2, 0, 1, 
-0, 1, 1, 
-1, 1, 1, 
-2, 1, 1, 
-0, 2, 1, 
-1, 2, 1, 
-2, 2, 1, 
-0, 3, 1, 
-1, 3, 1, 
-2, 3, 1, 
-0, 4, 1, 
-1, 4, 1, 
-2, 4, 1, 
-0, 0, 2, 
-1, 0, 2, 
-2, 0, 2, 
-0, 1, 2, 
-1, 1, 2, 
-2, 1, 2, 
-0, 2, 2, 
-1, 2, 2, 
-2, 2, 2, 
-0, 3, 2, 
-1, 3, 2, 
-2, 3, 2, 
-0, 4, 2, 
-1, 4, 2, 
-2, 4, 2, 
-0, 0, 3, 
-1, 0, 3, 
-2, 0, 3, 
-0, 1, 3, 
-1, 1, 3, 
-2, 1, 3, 
-0, 2, 3, 
-1, 2, 3, 
-2, 2, 3, 
-0, 3, 3, 
-1, 3, 3, 
-2, 3, 3, 
-0, 4, 3, 
-1, 4, 3, 
-2, 4, 3, 
-0, 0, 4, 
-1, 0, 4, 
-2, 0, 4, 
-0, 1, 4, 
-1, 1, 4, 
-2, 1, 4, 
-0, 2, 4, 
-1, 2, 4, 
-2, 2, 4, 
-0, 3, 4, 
-1, 3, 4, 
-2, 3, 4, 
-0, 4, 4, 
-1, 4, 4, 
-2, 4, 4, 
-0, 0, 5, 
-1, 0, 5, 
-2, 0, 5, 
-0, 1, 5, 
-1, 1, 5, 
-2, 1, 5, 
-0, 2, 5, 
-1, 2, 5, 
-2, 2, 5, 
-0, 3, 5, 
-1, 3, 5, 
-2, 3, 5, 
-0, 4, 5, 
-1, 4, 5, 
-2, 4, 5, 
-0, 0, 6, 
-1, 0, 6, 
-2, 0, 6, 
-0, 1, 6, 
-1, 1, 6, 
-2, 1, 6, 
-0, 2, 6, 
-1, 2, 6, 
-2, 2, 6, 
-0, 3, 6, 
-1, 3, 6, 
-2, 3, 6, 
-0, 4, 6, 
-1, 4, 6, 
-2, 4, 6, 
-*/
+  // cout << "my_mesh product_size: " << my_mesh->sizes_product() << endl; 
+
+  // // how to iterate through dynamic multi dim meshes?
+  // std::size_t end = my_mesh->sizes_product();  
+  // std::vector<double> coords(my_mesh->dims()); 
+  // for(std::size_t flat_i=0; flat_i<end; flat_i++){
+  //   for(std::size_t dim=0; dim<my_mesh->dims(); dim++){
+  //     std::size_t dim_i = flat_i;
+  //     dim_i /= my_mesh->sizes_partial_product(dim); 
+  //     dim_i %= my_mesh->dim_size(dim); 
+  //     cout << dim_i; 
+  //     cout << ", ";
+  //   }
+  //   cout << endl; 
+  // }
