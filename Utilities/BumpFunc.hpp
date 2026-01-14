7d5ad68ad186915b0d170b9d4a02e9ff3af7a6c9
Jack Feds <jackf10oh@gmail.com>
2026-01-14 16:15:15 -0500

    BumpFunc utility added for ICs

---
+++ Utilities/BumpFunc.hpp
@@ -0,0 +1,32 @@
+// BumpFunc.hpp
+//
+// Bump function on [L,R] with center c and height h
+//
+// JAF 1/13/2026
+
+#ifndef BUMPFUNC_H
+#define BUMPFUNC_H 
+
+struct BumpFunc
+{
+    double L = 0.0;
+    double R = 1.0; 
+    double c = 0.5;
+    double h = 1.0; 
+    double focus = 3.0; 
+    
+    // Member Funcs 
+    double operator()(double x)
+    {
+      if( c<L || x<L ||c>R || x>R) throw std::runtime_error("Error: Bad args to BumpFunc operator().");
+      // get a,b in Beta(a,b) distribution  
+      double a = focus * ((c-L)/(R-L)); 
+      double b = focus * ((R-c)/(R-L)); 
+      // scaling coefficient to make pdf(mode) == h
+      double scale = h / (std::pow(R*(a/(a+b)), a)*std::pow(R*(b/(a+b)), b)); 
+      // value of h*Beta(a,b) at (x-L) / (R-L) 
+      return scale * std::pow(x-L,a) * std::pow(R-x+L,b); 
+    }
+};
+
+#endif // BumpFunc.hpp
--- tests.cpp
+++ tests.cpp
@@ -12,11 +12,11 @@
 #include<Eigen/Dense>
 
 #include "Utilities/PrintVec.hpp"
+#include "Utilities/BumpFunc.hpp"
 
-#include "FDStencils/All.hpp" // must include first for plugin to take effect over linops?
+#include "FDStencils/All.hpp" // must be first for plugin macro. 
 #include "LinOps/All.hpp" 
-#include "FDStencilsXD/FdmPluginXD.hpp"
-#include "FDStencilsXD/All.hpp"
+#include "FDStencilsXD/All.hpp" // likewise ...
 #include "LinOpsXD/All.hpp"
 
 using std::cout, std::endl;
@@ -24,31 +24,34 @@ using std::cout, std::endl;
 using namespace Fds; 
 using namespace LinOps; 
 
-auto lam00 = [](){return 0.0;}; 
-auto lam01 = [](double x){return x*x - x + 1.5;}; 
-auto lam02 = [](double x, double y){return std::sqrt(x*x + y*y);}; 
-auto lam03 = [](double x, double y, double z){return std::sqrt(x*x + y*y + z*z);}; 
-
 int main()
 {
   // iomanip 
-  std::cout << std::setprecision(2); 
+  std::cout << std::setprecision(3); 
 
-  // auto my_meshes = LinOps::make_meshXD(0.0,1.0, 3, 3);
-  auto my_meshes = LinOps::make_mesh(0.0,3.0, 10);
+  // defining Domain + Time  Mesh --------------------------------------
+  auto r = 10.0; 
+  int n_gridpoints = 21;
+  // mesh in space 
+  auto my_mesh = Fds::make_mesh(0.0,r,n_gridpoints); 
+  // mesh in time 
+  auto time_mesh = Fds::make_mesh(0.0, 5.0, 101); 
 
-  Discretization1D disc; 
-  disc.set_init(my_meshes, lam01); 
+  // Making Discretization + setting ICs ----------------------------- 
+  Discretization1D my_vals; 
+  BumpFunc f{.L = 0.0, .R=r, .c=7.5, .h=4.0}; 
+  my_vals.set_init(my_mesh, f); 
 
-  cout << disc.values().transpose() << endl; 
-
-
-  auto row = disc.values().transpose(); 
-  cout << "cycle entries" << endl << SparseDiag<decltype(row), SparseDiagPattern::CYCLE>(row, 2) << endl; 
-
-  cout << "repeat entries" << endl << SparseDiag(row,2) << endl; 
-
-  // cout << R.GetMat().toDense() << endl; 
+  // Printing ----------------------------------------------
+  print_vec(my_vals, "Bump"); 
 
 };
 
+// auto get_interval = [](const auto& v, double c){
+//   if(v.size() < 2) throw std::runtime_error("size of v < 2"); 
+//   if(c < v.cbegin()[0]) throw std::runtime_error("c < v[0]"); 
+//   auto after = std::lower_bound(v.cbegin(), v.cend(), c);
+//   if(after == v.cbegin()) return std::pair(after, after+1); 
+//   if(after == v.cend()) throw std::runtime_error("right bound == v.cend()") 
+//   return std::pair(after-1, after); 
+// } 
