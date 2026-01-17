6b7ad74969cfebf2f8bf771a962e89ad165f53ec
Jack Feds <jackf10oh@gmail.com>
2026-01-17 16:49:50 -0500

    GeneralSolver class added. working with 1D classes

--- LhsExpressions/GeneralSolver.hpp
+++ LhsExpressions/GeneralSolver.hpp
@@ -7,6 +7,8 @@
 #ifndef GENERALSOLVER_H
 #define GENERALSOLVER_H 
 
+#include "../LinOps/Mesh.hpp"
+
 template<typename ANYMESH_SHAREDPTR_T, typename ANYBC_SHAREDPTR_T, typename CONTAINER_T>
 struct GenSolverArgs
 {
@@ -15,7 +17,7 @@ struct GenSolverArgs
   ANYMESH_SHAREDPTR_T domain_mesh_ptr; 
 
   // shared_ptr to const Mesh1D 
-  std::shared_ptr<const Mesh1D> time_mesh_ptr; 
+  std::shared_ptr<const LinOps::Mesh1D> time_mesh_ptr; 
 
   // shared_ptr to BcPtr_t or BCXDPtr_t
   ANYBC_SHAREDPTR_T bcs;
@@ -27,21 +29,88 @@ struct GenSolverArgs
   bool time_dep_flag=true; 
 }; 
 
+// CTAD Guide 
+template<typename M, typename B, typename C>
+GenSolverArgs(M, std::shared_ptr<const LinOps::Mesh1D>, B, C, bool)
+-> GenSolverArgs<M, B, C>;
+
 template<typename LHS_EXPR, typename RHS_EXPR>
 class GenSolver
 {
-  // Member Data -------------------------------------
-  LHS_EXPR& m_lhs; // expression of time derivatives 
-  RHS_EXPR& m_rhs; // expression of spatial derivatives 
+  private:
+    // Member Data -------------------------------------
+    LHS_EXPR& m_lhs; // expression of time derivatives 
+    RHS_EXPR& m_rhs; // expression of spatial derivatives 
 
-  // Constructors + Destructor ===========================
-  GenSolver()=delete; 
-  GenSolver(LHS_EXPR& l_init, RHS_EXPR& r_init)
-    : m_lhs(l_init), m_rhs(r_init)
-  {}
-  GenSolver(const GenSolver& other)=delete;
-  // destructor 
-  ~GenSolver()=default;  
+  public:
+    // Constructors + Destructor ===========================
+    GenSolver()=delete; 
+    GenSolver(LHS_EXPR& l_init, RHS_EXPR& r_init)
+      : m_lhs(l_init), m_rhs(r_init)
+    {}
+    GenSolver(const GenSolver& other)=delete;
+    // destructor 
+    ~GenSolver()=default;  
+
+    // Member Funcs =================================================
+    template<typename... Ts>
+    auto Calculate(const GenSolverArgs<Ts...>& args)
+    {
+
+      LhsExecutor exec(m_lhs); 
+
+      exec.set_mesh(args.domain_mesh_ptr);
+      m_rhs.set_mesh(args.domain_mesh_ptr); 
+
+      auto it = args.time_mesh_ptr->cbegin() + args.ICs.size(); 
+      auto end = args.time_mesh_ptr->cend(); 
+
+      exec.ConsumeSolutionList(args.ICs.cbegin(), args.ICs.cend()); 
+      exec.ConsumeTimeList(args.time_mesh_ptr->cbegin(), it); 
+
+      switch (args.time_dep_flag)
+      {
+      case true:
+        for(; it!= end; it++)
+        {
+          m_rhs.SetTime(*it);
+          exec.SetTime(*it); 
+
+          Eigen::VectorXd rhs = exec.BuildRhs(*it); 
+          
+          // Explicit Step 
+          Eigen::VectorXd next_sol = exec.inv_coeff_util()*m_rhs.GetMat()*exec.MostRecentSol() + rhs; 
+          
+          // Apply BCs 
+          args.bcs->SetSol(next_sol, args.domain_mesh_ptr); 
+
+          // push Solution, time to executor 
+          exec.ConsumeSolution(next_sol);
+          exec.ConsumeTime(*it); 
+        }
+        break;
+      
+      default: // false 
+        for(; it!= end; it++)
+        {
+          Eigen::VectorXd rhs = exec.BuildRhs(*it); 
+          
+          // Explicit Step 
+          Eigen::VectorXd next_sol = exec.inv_coeff_util()*m_rhs.GetMat()*exec.MostRecentSol() + rhs; 
+          
+          // Apply BCs 
+          args.bcs->SetSol(next_sol, args.domain_mesh_ptr); 
+
+          // push Solution, time to executor 
+          exec.ConsumeSolution(next_sol);
+          exec.ConsumeTime(*it); 
+        }
+        break;
+      } // end switch(args.time_dep_flag)
+
+      // give last solution as result
+      return std::move(exec.MostRecentSol()); 
+    }
 }; 
 
 #endif
---
+++ main4.cpp
@@ -0,0 +1,80 @@
+// main4.cpp 
+//
+// Testing out GenSolver on PDEs 
+//
+// JAF 1/17/2026 
+
+#include<cstdint>
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<tuple>
+#include<Eigen/Dense>
+
+#include "Utilities/PrintVec.hpp"
+#include "Utilities/BumpFunc.hpp"
+
+#include "FDStencils/All.hpp" // must be first for plugin macro. 
+#include "LinOps/All.hpp" 
+// #include "FDStencilsXD/All.hpp" // likewise ...
+// #include "LinOpsXD/All.hpp"
+
+#include "LhsExpressions/All.hpp"
+
+using std::cout, std::endl;
+
+using namespace Fds; 
+using namespace LinOps; 
+
+int main()
+{
+  // iomanip 
+  std::cout << std::setprecision(3); 
+
+  // defining Domain Mesh --------------------------------------
+  auto r = 10.0; 
+  int n_gridpoints = 21;
+  // mesh in space 
+  auto my_mesh = Fds::make_mesh(0.0,r,n_gridpoints); 
+  // mesh in time 
+  auto time_mesh = Fds::make_mesh(0.0, 5.0, 501); 
+
+  // Initializing IC discretizations -------------------------------------------------------
+  Fds::Discretization1D my_vals;
+
+  // Bump centered at (r/2,r2). Zero at x=0, x=r, y=0, y=r. 
+  BumpFunc f{.L=0.0, .R=r, .c=r/2, .h=1.0, .focus=20.0};
+  my_vals.set_init(my_mesh, f); 
+
+  // building RHS expression -----------------------------------------------------
+  using D = Fds::NthDerivOp;
+  auto expr = 0.2 * D(2); 
+  // auto expr = 0.2 * D(2) - 0.5 * D(1); 
+
+  // Boundary Conditions + --------------------------------------------------------------------- 
+  std::shared_ptr<Fds::IBCLeft> left = Fds::make_dirichlet(0.0); 
+  std::shared_ptr<Fds::IBCRight> right = Fds::make_dirichlet(0.0); 
+
+  auto bcs = std::make_shared<BCPair>(left,right); 
+
+  // LHS time derivs ----------------------------------------------------------------
+  // auto lhs_expr = NthTimeDeriv(1); 
+  auto lhs_expr = NthTimeDeriv(1); 
+
+  // Solving --------------------------------------------------------------------- 
+  GenSolverArgs args{
+    .domain_mesh_ptr = my_mesh,
+    .time_mesh_ptr = time_mesh,
+    .bcs = bcs, 
+    .ICs = std::vector<Eigen::VectorXd>(1, my_vals.values()), 
+    .time_dep_flag = false 
+  }; 
+
+  GenSolver s(lhs_expr, expr); 
+  auto v = s.Calculate(args); 
+
+  // Printing ---------------------------------------------------------------- 
+  print_vec(my_vals, "ICs"); 
+  print_vec(v,"Sol"); 
+  // auto foo = exec.BuildRhs(*it); 
+};
