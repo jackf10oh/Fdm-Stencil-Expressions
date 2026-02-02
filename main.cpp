18c8705e5dcbfb026894319672a30f34d7dc3bbf
Jack Feds <jackf10oh@gmail.com>
2026-02-01 21:17:25 -0500

    fixed formula for BCList in OutsideSteps. just using sizes_middle_product. no cumulative products...

--- main.cpp
+++ main.cpp
@@ -33,28 +33,29 @@ int main()
   std::cout << std::setprecision(2); 
 
   double t = 0.0; 
-  auto mesh = LinOps::make_meshXD(0.0,1.0,4,3); 
+  std::vector<std::pair<double,double>> ends = {{0.0,1.0},{0.0,1.0},{0.0,1.0}}; 
+  std::vector<std::size_t> steps = {6,6,4};  
+  auto mesh = LinOps::make_meshXD(ends, steps); 
 
-  auto v = LinOps::DiscretizationXD().set_init(mesh, [](double x, double y){ return x + y*y; }).values(); 
+  // auto v = LinOps::DiscretizationXD().set_init(mesh, [](double x, double y){ return x + y*y; }).values(); 
 
-  print_mat(mesh->OneDim_views(v), "OneDim Views(0)"); 
+  // print_mat(mesh->OneDim_views(v), "OneDim Views(0)"); 
 
-  auto bc1 = OSteps::NeumannBC(1.0); 
+  auto bc1 = OSteps::DirichletBC(1.0); 
   auto bc2 = OSteps::NeumannBC(2.0); 
   auto bc3 = OSteps::DirichletBC(3.0); 
   // auto bcs_list = OSteps::BCList( p(bc1,bc1), p(bc2,bc2) ); 
   auto bcs_list = OSteps::BCList( p(bc1,bc1), p(bc2,bc2), p(bc3,bc3) ); 
 
   // bcs_list.SolAfterStep<OSteps::FDStep_Type::EXPLICIT>(t,mesh,v); 
-  bcs_list.SolBeforeStep<OSteps::FDStep_Type::IMPLICIT>(t,mesh,v); 
-  print_mat(mesh->OneDim_views(v), "OneDim Views(0)"); 
+  // bcs_list.SolBeforeStep<OSteps::FDStep_Type::IMPLICIT>(t,mesh,v); 
+  // print_mat(mesh->OneDim_views(v), "OneDim Views(0)"); 
 
 
-  LinOps::MatrixStorage_t R = (5 * LinOps::IOp(mesh)).GetMat(); 
-  cout << "R" << endl << R << endl; 
+  LinOps::MatrixStorage_t R = (22 * LinOps::IOp(mesh)).GetMat(); 
+  // cout << "R" << endl << R << endl; 
 
   bcs_list.MatBeforeStep(t,mesh,R); 
   cout << "R" << endl << R << endl; 
-
 
 };
